3b22ff71b057aa71dbc6ea4d49e5ed270c0b71f0
Paniagua96 <paniagua-11@hotmail.com>
2019-05-21 20:15:26 -0500

    Se hizo script de musica falta implementarlo en sonido 3d

---
+++ Assets/Scrpts/ManagerSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerSound : MonoBehaviour
+{
+public AudioSource nivel1;
+public AudioSource nivel2;
+public AudioSource nivel3;
+public AudioSource efectoViento;
+public AudioSource efectoFuego;
+public AudioSource efectoSierra;
+public AudioSource efectoCampanas;
+public AudioSource efectoPasosTierra;
+public AudioSource efectoArboles;
+public AudioSource efectoGrillos;
+public AudioSource efectoRespiracion;
+public AudioSource efectoLechuza;
+public AudioSource efectoGallo;
+
+
+public void activateMusic(AudioSource music)
+{
+music.Play();
+music.loop = true;
+}
+}
---
+++ Assets/Scrpts/UseEffects.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseEffects : MonoBehaviour
+{
+    public ManagerSound managerSound;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        playEffect(other.tag == "Arboles", managerSound.efectoArboles,true);
+        playEffect(other.tag == "Arboles", managerSound.efectoArboles, true);
+        playEffect(other.tag == "Arboles", managerSound.efectoArboles, true);
+        playEffect(other.tag == "Arboles", managerSound.efectoArboles, true);
+        playEffect(other.tag == "Arboles", managerSound.efectoArboles, true);
+        playEffect(other.tag == "Arboles", managerSound.efectoArboles, true);
+        playEffect(other.tag == "Arboles", managerSound.efectoArboles, true);
+        playEffect(other.tag == "Arboles", managerSound.efectoArboles, true);
+        playEffect(other.tag == "Arboles", managerSound.efectoArboles, true);
+        playEffect(other.tag == "Arboles", managerSound.efectoArboles, true);
+    }
+
+
+
+    public void playEffect(bool tag, AudioSource effect, bool isLoop)
+    {
+        if (tag)
+        {
+            effect.Play();
+            effect.loop = isLoop;
+        }
+    }
+
+
+
+}
